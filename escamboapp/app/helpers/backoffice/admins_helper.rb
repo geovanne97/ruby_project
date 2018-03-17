703d55d312b8b361d6f1a51fbfbf516126f0bb83
geovanne <geovannessaraiva97@gmail.com>
2018-03-17 15:41:42 -0300

    collection radio buttons

--- escamboapp/app/helpers/backoffice/admins_helper.rb
+++ escamboapp/app/helpers/backoffice/admins_helper.rb
@@ -1,2 +1,13 @@
 module Backoffice::AdminsHelper
+  OptionsForRoles = Struct.new(:id, :description)
+
+  def options_for_roles
+    options = []
+    #this method he takes the 2 roles that admin have "full acess and restricted" and push this struct in options array
+    Admin.roles_i18n.each do |key, value|
+      options.push(OptionsForRoles.new(key, value))
+    end
+    #this option will return the array with the 2 tips of struct 
+    options
+  end
 end
