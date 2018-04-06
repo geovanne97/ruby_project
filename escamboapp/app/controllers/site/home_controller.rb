e108a002a809561835248429b8efa8b2fa5a3e61
geovanne <geovannessaraiva97@gmail.com>
2018-04-06 00:09:11 -0300

    refactoring

--- escamboapp/app/controllers/site/home_controller.rb
+++ escamboapp/app/controllers/site/home_controller.rb
@@ -2,7 +2,7 @@ class Site::HomeController < ApplicationController
   layout "site"
 
   def index
-    @categories = Category.order(:description)#if i want so order in decrescente order i use description: :desc
-    @ads = Ad.limit(5).order(created_at: :desc)#he will list the anounce 5 by 5 and in desc order 
+    @categories = Category.order_by_description#if i want so order in decrescente order i use description: :desc
+    @ads = Ad.last_six  #he will call the scope in ad 
   end
 end
